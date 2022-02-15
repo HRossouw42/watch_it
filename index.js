a571f3671e6902458e63d4cb115bbbccf9b1dcd5
Harmun <harmun.rossouw@coil.com>
2022-02-15 12:47:16 +0200

    finalised

--- index.js
+++ index.js
@@ -3,13 +3,23 @@
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
 const { program } = require('@caporal/core');
+const fs = require('fs');
 
 const log = console.log.bind(console);
 
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of file to execute')
-  .action(({ args }) => {
+  .action(async ({ args }) => {
+    // check if file exists
+    const name = args.filename || 'index.js';
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find file: ${name}`);
+    }
+
+    // debounce function to prevent file added spam
     const start = debounce(() => {
       console.log('Starting user program');
     }, 100);
@@ -17,7 +27,7 @@ program
     // One-liner for current directory
     chokidar
       .watch('.')
-      .on('add', start) // debounced to not spam for every file added as program starts
+      .on('add', start)
       .on('change', (path) => log(`File ${path} has been changed`))
       .on('unlink', (path) => log(`File ${path} has been removed`));
   });
